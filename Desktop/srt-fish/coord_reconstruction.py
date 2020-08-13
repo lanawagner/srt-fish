2241e1d203ecd3e50036389c22457ca901fb4695
Lana Wagner <l.rose3002@gmail.com>
2020-08-13 16:22:48 -0400

    updated coord reconstruction to work on video feed and updated inverse function

--- Desktop/srt-fish/coord_reconstruction.py
+++ Desktop/srt-fish/coord_reconstruction.py
@@ -4,26 +4,35 @@ from picamera import PiCamera
 from time import sleep
 from matplotlib import pyplot as plt
 import cv2
+import time
 import argparse
 import os
 import imutils
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from imutils.video import VideoStream
+from imutils.video import FPS
 from imutils import paths
 from PIL import Image
 import numpy as np;
 
+#camera video setup
+vs = VideoStream(usePiCamera=True).start()
+time.sleep(2.0)
+fps = FPS().start()
+print("STARTING VIDEO FEED")
 
 # LOOP CODE
-#reading all the files in crimages
-directory = '/home/pi/Desktop/crimages/'
-for filename in os.listdir(directory): #return filename + size
+while True: #constant video frame read
+    frame = vs.read()
     
     #read image, rotate
-    pathname= os.path.join(directory, filename)
-    im=cv2.imread(pathname)
-    im = cv2.rotate(im, cv2.ROTATE_180)
+#     pathname= os.path.join(directory, filename)
+#     im=cv2.imread(pathname)
+#     im = cv2.rotate(im, cv2.ROTATE_180)
     
     #convert to HSV, set HSV boundaries
-    hsv=cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     boundaries = [
         ([7,120,120], [153,255,255])#ORANGE, OpenCV measures HSV from 0-255 range
         #HSV in 0-100 range: [3,30,30], [60,100,100] #OLD: [7,76,76], [153,255,255]
@@ -47,31 +56,48 @@ for filename in os.listdir(directory): #return filename + size
     if len(cnts) > 0:
         c=max(cnts, key=cv2.contourArea)
         ((x,y),radius)= cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center=(int(M["m10"]/M["m00"]),int(M["m01"]/M["m00"]))
-        cv2.circle(im, (int(x), int(y)), int(radius), (0,255,0), 2) #BOUNDING CIRCLE
-        cv2.circle(im, center, 5, (255,0,0),-1) #CENTER
+#         M = cv2.moments(c)
+#         center=(int(M["m10"]/M["m00"]),int(M["m01"]/M["m00"]))
+#         #drawing here:
+        cv2.circle(frame, (int(x), int(y)), int(radius), (255,0,0), 2) #BOUNDING CIRCLE
+#         cv2.circle(frame, center, 5, (255,0,0),-1) #CENTER
         
         
-    cx=960 #center x
-    cy=540 #center y
+    #Photo dimensions: (1920,1080); Center coords: 960,540
+    #Video Feed dimensions: (320,240); Center coords: 160,120
+#     cx=160 #center x, photo: 960
+#     cy=120 #center y, photo: 540
     imagesize=radius*2
-    u=x-cx
-    v=y-cy
-    v=-v #because reference frame is top left
-    cv2.circle(im, (int(cx), int(cy)), 5, (0,0,255),-1) #drawing the image center point
+#     u=x-cx
+#     v=y-cy
+#     v=-v #because reference frame is top left
+#     cv2.circle(frame, (int(cx), int(cy)), 5, (0,0,255),-1) #drawing the image center point
     
     #calculate distance:
-    physicalz=(imagesize/2276)**(1/(-0.976))
-    #real life coords:
-    f=2714.29 #focal length in px
-    physicalx=(physicalz/f)*u
-    physicaly=(physicalz/f)*v
-    
-    print(pathname, " Image Size: ", str(imagesize), " Physical Distance: ", str(physicalz), " (u,v): (", u, ",", v, ")", " Real (X,Y,Z): (", physicalx, ",", physicaly,",", physicalz, ")")
+    physicalz=(imagesize/473)**(1/(-1.07))
+#     #real life coords:
+#     f=2714.29 #focal length in px
+#     physicalx=(physicalz/f)*u
+#     physicaly=(physicalz/f)*v
+
+#     sizetext=imagesize, " px wide"
+#     print(sizetext)
+    cv2.putText(frame, str(imagesize), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
+#     print("Image Size: ", str(imagesize), " Physical Distance (approx): ", str(physicalz))
+          #, " (u,v): (", u, ",", v, ")", " Real (X,Y,Z): (", physicalx, ",", physicaly,",", physicalz, ")")
     #print("Center: (", str(x), ",",str(y),")")
-    cv2.imshow(pathname, im)
-    cv2.waitKey(0)
+    cv2.imshow("Frame", frame)
+    key= cv2.waitKey(5)# & 0xFF
+    
+    #to exit stream
+    if key == 27: #esc key #ord("q"):
+        print("esc key pressed")
+        break
+    
+    fps.update()
 
-#center coords: (960,540) (I THINK) (dimensions are (1920,1080)?)
+#Stream end protocol:
+print("VIDEO FEED STOPPED")
 
+cv2.destroyAllWindows()
+vs.stop()
